77a36be4fbe3efb07c178d809262a77db34a9183
LKJUnity3 <154484433+LKJUnity3@users.noreply.github.com>
2024-01-25 09:42:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for (int i = 0; n > 0; i++)
+        {
+            answer += n % 10;
+            n = n / 10;
+        }
+        
+        return answer;
+    }
+}
